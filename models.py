2af7966e23910e08f6baba30946a3a65a5c0a598
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-10-26 15:23:54 -0500

    Add columns and update other columns with data type, primary and foreign key info, and nullable info

--- models.py
+++ models.py
@@ -24,6 +24,12 @@ class User(db.Model):
         unique=True,
     )
 
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
     password = db.Column(
         db.Text,
         nullable=False,
@@ -39,8 +45,17 @@ class User(db.Model):
         nullable=False,
     )
 
+    image_url = db.Column(
+        db.Text,
+        default="/static/images/default-pic.png",
+    )
+
+    # def __repr__(self):
+    #     """Create a readable, identifiable representation of user."""
+    #     return f"<User #{self.id}: {self.username}, {self.email}>"
+
     @classmethod
-    def signup(cls, username, password, first_name, last_name):
+    def signup(cls, email, username, password, first_name, last_name, image_url):
         """Sign up user.
         Hashes password and adds user to system."""
 
@@ -51,6 +66,8 @@ class User(db.Model):
             password=hashed_pw,
             first_name=first_name,
             last_name=last_name,
+            image_url=image_url,
+            email=email,
         )
 
         db.session.add(user)
@@ -70,7 +87,7 @@ class User(db.Model):
         user = cls.query.filter_by(username=username).first()
 
         if user:
-            is_auth = bcrypt.check_password_hash(user.password, password)
+            is_auth = bcrypt.check_password_hash(user.hashed_pw, password)
             if is_auth:
                 return user
 
@@ -80,44 +97,105 @@ class User(db.Model):
 class Course(db.Model):
     """Course that is made up of videos curated by course creator"""
 
-    id = db.Column(
+    # should I keep this id?
+    # if yes, how to make the id autoincrement?
+    # id = db.Column(
+    #     db.Integer,
+    # )
+
+    popularity = db.Column(
         db.Integer,
-        primary_key=True
+        nullable=False,
+        default=0,
     )
 
     title = db.Column(
-
+        db.Text,
+        primary_key=True,
     )
 
     creator_id = db.Column(
-
+        db.Integer,
+        db.ForeignKey('users.id'),
+        primary_key=True,
     )
 
 
 class Video(db.Model):
     """Video information and data"""
 
+    # id comes from YouTube Data API
     id = db.Column(
-        db.Integer,
+        db.Text
         primary_key=True,
     )
 
     title = db.Column(
-
+        db.Text,
+        nullable=False,
     )
 
-    you_tube_creator_id = db.Column(
+    description = db.Column(
+        db.Text,
+    )
 
+    yt_channel_id = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    yt_channel_title = db.Column(
+        db.Text,
+    )
+
+    iframe_html = db.Column(
+        db.Text,
+    )
+
+    viewCount = db.Column(
+        db.Integer,
+    )
+
+    likeCount = db.Column(
+        db.Integer,
+    )
+
+    pctlike = db.Column(
+        db.Integer,
     )
 
 
 class Subscription(db.Model):
     """Join table for users and courses"""
 
-    user_id = db.Column(
+    subscriber_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete="cascade"),
+        primary_key=True,
+    )
 
+    course_id = db.Column(
+        db.Integer,
+        db.ForeignKey('courses.id', ondelete="cascade"),
+        primary_key=True,
     )
 
 
-class (db.Model):
-    """Join table for users and courses"""
+class VideosCourses(db.Model):
+    """Join table for videos and courses"""
+
+    course_id = db.Column(
+        db.Integer,
+        db.ForeignKey('courses.id', ondelete="cascade"),
+        primary_key=True,
+    )
+
+    video_id = db.Column(
+        db.Text,
+        db.ForeignKey('videos.id', ondelete="cascade"),
+        primary_key=True,
+    )
+
+    video_seq = db.Column(
+        db.Integer,
+    )
