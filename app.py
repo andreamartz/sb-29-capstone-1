d2f83ed4a5750511dd4dc45eff1998e34f9a76dc
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-10-29 12:08:32 -0500

    Add the 404 route and the homepage route

--- app.py
+++ app.py
@@ -194,6 +194,25 @@ def search_videos():
 
     return res
 
+
+# ************************************
+# OTHER ROUTES
+# ************************************
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Handle 404 errors by showing custom 404 page."""
+
+    return render_template('404.html'), 404
+
+
+@app.route("/")
+def homepage():
+    """Show homepage."""
+
+    return render_template('home.html')
+
+
 # *******************************
 # USER ROUTES
 # *******************************
