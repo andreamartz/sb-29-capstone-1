b7259730b3144447ecf44c117f0df79170c275fb
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-10-30 13:12:53 -0500

    Add csv files and seed file to generate data for users and courses

---
+++ seed.py
@@ -0,0 +1,26 @@
+"""Seed database with sample data from CSV Files."""
+
+import csv
+from csv import DictReader
+from app import db
+from models import User, Course
+
+
+db.drop_all()
+db.create_all()
+
+print('************************')
+
+with open('generator/users.csv') as users:
+    print('hello *********')
+    print(DictReader(users))
+
+    db.session.bulk_insert_mappings(User, DictReader(users))
+
+with open('generator/courses.csv') as courses:
+    reader = csv.DictReader(courses)
+    for row in reader:
+        print(row)
+    db.session.bulk_insert_mappings(Course, DictReader(courses))
+
+db.session.commit()
