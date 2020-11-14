b7c306ff570911c1cf26ed2bf6ae744c27ea948a
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-11-14 16:54:08 -0600

    Set backup secret key in app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "another543256432secret")
 
 toolbar = DebugToolbarExtension(app)
 
